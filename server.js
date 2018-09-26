30e2c440fe6aa44c0a95da7017fb30fe586cbf21
willshepp28 <willsheppard28@outlook.com>
2018-09-26 15:51:36 -0400

    file destination changes in server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ application.listen(process.env.PORT || 8080);
 // PathLocationStategy
 
 application.get("/*", function(request, response) {
-    response.sendFile(path.join(__dirname + "/dist/index.html"));
+    response.sendFile(path.join(__dirname + "/dist/IGclone/index.html"));
 });
 
 console.log("Console listening");
