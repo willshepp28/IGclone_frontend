6c9180b1f5efa5b408613277d2a2127e6c1fa246
willshepp28 <willsheppard28@outlook.com>
2018-10-03 16:42:58 -0400

    changed server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require("path");
 application.use(express.static(__dirname + "/dist/frontend"));
 
 
-application.listen(process.env.PORT || 8080);
+
 
 
 // PathLocationStategy
@@ -14,6 +14,9 @@ application.listen(process.env.PORT || 8080);
 application.get("/*", function(request, response) {
     response.sendFile(path.join(__dirname + "/dist/frontend/index.html"));
 });
+
+
+application.listen(process.env.PORT || 8080);
 
 console.log("Server listening");
 
