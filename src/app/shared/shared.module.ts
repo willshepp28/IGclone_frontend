1af3ab4eadbb420668cc8245fc0569d8dfa2cf2c
willshepp28 <willsheppard28@outlook.com>
2018-10-20 12:09:58 -0400

    made a shared folder. And changed favicon

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+
+
+/**
+ * The Shared Module holds everything that will be shared in our app
+ */
+
+
+
+@NgModule({
+    declarations: [],
+    exports: []
+})
+export class SharedModule {}
